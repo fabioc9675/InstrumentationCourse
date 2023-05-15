c05ebf5a364eb1a4704c7e8a4543eba1948deed8
Fabian Castaño <fabioc9675@gmail.com>
2023-05-15 15:22:11 -0500

    Addition of MongoDB library connection and CORS

--- Project_20231/instrumentation/src/index.js
+++ Project_20231/instrumentation/src/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
 
 const app = express(); // server
+require("dotenv").config();
 
 // Assignacion de puerto
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
+
+// Uso de CORS para la seguridad de la aplicacion
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 // Llamado a la aplicacion
 app.use((req, res) => {
-    res.send("Hola Mundo, soy Fabian!!!");
+    res.send("<html><body><h1>Hola Mundo</h1></body></html>");
 });
 
 // Configuracion
