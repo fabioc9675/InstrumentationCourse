c0047b27c72902c57f5306dd9653afbe27dc02f9
Fabian Castaño <fabioc9675@gmail.com>
2023-05-15 15:30:57 -0500

    Connection with DataBase

--- Project_20231/instrumentation/src/index.js
+++ Project_20231/instrumentation/src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express(); // server
 require("dotenv").config();
 
 // Assignacion de puerto
+const URI = process.env.DB_URI;
 const port = process.env.PORT || 3000;
+const connection = mongoose.connection;
 
 // Uso de CORS para la seguridad de la aplicacion
 app.use((req, res, next) => {
@@ -16,6 +18,8 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.json()); // todo dato que viene al servidor es en formato JSON
+
 // Llamado a la aplicacion
 app.use((req, res) => {
     res.send("<html><body><h1>Hola Mundo</h1></body></html>");
@@ -23,6 +27,12 @@ app.use((req, res) => {
 
 // Configuracion
 app.set("port", port); // toma el puerto configurado y se lo asigna a la app
+
+// Conectar a DB
+mongoose
+    .connect(URI) // creacion de la conexion a la base de datos
+    .then((db) => console.log("Database is connected"))
+    .catch((err) => console.error(err));
 
 app.listen(app.get("port"), () => {
     console.log(`Servidor en el puerto ${app.get("port")}`);
